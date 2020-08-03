756d25cd5a12ac97c1b9c67546848391518b496e
naonaorange <nao.lunalovegood@gmail.com>
2020-08-03 17:09:06 +0900

    fix the error

--- lib/bacoflea.dart
+++ lib/bacoflea.dart
@@ -269,7 +269,7 @@ class ProductSearcher{
   get productName => _productName;
 
   ProductSearcher(){
-    //this._clientId = 'CLIENT_ID';
+    this._clientId = 'CLIENT_ID';
   }
 
   Future search(String janCode) async {
